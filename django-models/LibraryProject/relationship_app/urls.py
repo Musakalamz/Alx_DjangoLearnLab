62bebcbac3596c245565555fc9eec22557708016
Musakalamz <ogunsolumusa@gmail.com>
2026-01-16 23:07:00 +0100

    Wire list_books import in relationship_app urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,9 +2,10 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
+from .views import list_books
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
+    path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
