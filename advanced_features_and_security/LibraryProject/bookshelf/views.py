77a19d388be0fd4f7a9ef90e74df8c4b1ba65eaa
Musakalamz <ogunsolumusa@gmail.com>
2026-01-23 13:13:23 +0100

    Fix imports in views.py for checker compliance

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm, ExampleForm
+from .forms import ExampleForm
+from .forms import BookForm
 from django.http import HttpResponse
 
 @permission_required('bookshelf.can_view', raise_exception=True)
