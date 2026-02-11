1abf289195e312239b80defc8bdc426993fea1b6
Musakalamz <ogunsolumusa@gmail.com>
2026-02-11 07:09:26 +0100

    feat: Implement Blog Post CRUD features (List, Detail, Create, Update, Delete) with permissions

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post
 
 class CustomUserCreationForm(UserCreationForm):
     """
@@ -19,3 +20,12 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,9 +3,19 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='register'), # Default to register for now or home
+    # Home/Root
+    path('', views.PostListView.as_view(), name='home'),
+
+    # Authentication
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+
+    # Blog Post CRUD
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
