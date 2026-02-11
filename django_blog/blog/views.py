be36dd290f642eeec6c21f0f33b4374c1e5d1c24
Musakalamz <ogunsolumusa@gmail.com>
2026-02-11 20:42:49 +0100

    Fix-URL-configuration-for-tags-to-use-slug

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -138,12 +138,12 @@ class PostByTagListView(ListView):
     context_object_name = 'posts'
 
     def get_queryset(self):
-        tag_name = self.kwargs.get('tag_name')
-        return Post.objects.filter(tags__name__icontains=tag_name)
+        tag_slug = self.kwargs.get('tag_slug')
+        return Post.objects.filter(tags__slug=tag_slug)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tag'] = self.kwargs.get('tag_name')
+        context['tag'] = self.kwargs.get('tag_slug')
         return context
 
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
